f4e87b9dbc26026facfb7ef7a68beba2bd36b94a
unknown <logeshmyown1@gmail.com>
2025-07-27 16:33:29 +0530

    update

--- straxon.webflow.io/assets/js/slick-control.js
+++ straxon.webflow.io/assets/js/slick-control.js
@@ -22,4 +22,36 @@ $(document).ready(function(){
       }
     ]
   });
+
+
+   $('.choose-us-slider-services').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    dots: false,
+    autoplay: true,             
+    autoplaySpeed: 1500,       
+    responsive: [
+ {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
