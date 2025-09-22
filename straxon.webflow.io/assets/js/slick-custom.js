7a38cc9f31c8ab49b709daf0f63266a648e6df23
unknown <logeshmyown1@gmail.com>
2025-09-22 12:56:41 +0530

    Added all slick js and css in all pages

--- straxon.webflow.io/assets/js/slick-custom.js
+++ straxon.webflow.io/assets/js/slick-custom.js
@@ -1,57 +1,34 @@
-$(document).ready(function(){
-  $('.projec-slider').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    infinite: true,
-    arrows: true,
-    dots: false,
-    autoplay: true,             
-    autoplaySpeed: 1500,       
-    responsive: [
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1
-        }
-      }
-    ]
+ $(document).ready(function(){
+
+    // Slider 1
+    $('.projec-slider').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      dots: false,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      responsive: [
+        { breakpoint: 991, settings: { slidesToShow: 3 } },
+        { breakpoint: 767, settings: { slidesToShow: 1 } }
+      ]
+    });
+
+    // Slider 2
+    $('.choose-us-slider-services').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      dots: false,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      responsive: [
+        { breakpoint: 1024, settings: { slidesToShow: 3 } },
+        { breakpoint: 991, settings: { slidesToShow: 2 } },
+        { breakpoint: 767, settings: { slidesToShow: 1 } }
+      ]
+    });
+
   });
-
-
-   $('.choose-us-slider-services').slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    infinite: true,
-    arrows: true,
-    dots: false,
-    autoplay: true,             
-    autoplaySpeed: 1500,       
-    responsive: [
- {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3
-        }
-      },
-
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-});
