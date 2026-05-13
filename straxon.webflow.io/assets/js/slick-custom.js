8584575fa5d6b682a1c255b17b6cf300857c5b39
unknown <logeshmyown1@gmail.com>
2026-05-13 20:19:02 +0530

    Removed award sec

--- straxon.webflow.io/assets/js/slick-custom.js
+++ straxon.webflow.io/assets/js/slick-custom.js
@@ -45,23 +45,7 @@
 });
 
   // Initially show first tab
-  $('.home-two-faq-boxes').hide();
-  $('.home-two-faq-boxes-one').show();
 
-  $('.home-two-options').on('click', function(){
-    let index = $(this).index();
-
-    // Remove and add active class
-    $('.home-two-options').removeClass('act');
-    $(this).addClass('act');
-
-    // Animate accordion-line
-    $('.accordion-line').css('width', '0');
-    $(this).find('.accordion-line').css('width', '50px');
-
-    // Show corresponding left content
-    $('.home-two-faq-boxes').hide().eq(index).fadeIn(400);
-  });
 
  $('.tab-links').on('click', function () {
   $('.tab-links').removeClass('tp-active');  // remove from all
